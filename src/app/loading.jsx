20380eff43d9bc82ea5f01f281084358a84f95c7
unknown <you@example.com>
2024-03-10 22:12:00 +0600

    Add card component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import loadingSpinner from "../../public/spinner.svg";
+
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      <img className="h-52" src="spinner.svg" alt="loading..." />
+      {/* <img className="h-52" src={loadingSpinner} alt="loading..." /> */}
+      <Image
+        className="h-52"
+        width={52}
+        height={52}
+        src={loadingSpinner}
+        alt="loading..."
+      />
     </div>
   );
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Results({ results }) {
   return (
-    <div>
-      {results.map((result, index) => (
-        <div key={result.id}>
-          <h2>{result.original_title}</h2>
-        </div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4 w-full">
+      {results.map((result) => (
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
